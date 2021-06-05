37abd26b9ab8af0360d3b93736a20afcec81ed5a
Mike Nealon <chisao101@gmail.com>
2021-06-04 21:33:00 -0400

    six done! three more to go

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ const contentPick = document.querySelector('.content-pick');
 const contentDestination = document.querySelector('.ontent-destination');
 const container = document.querySelector('.container');
 
+// One
 // mouseover event
 // changes link color and bg color
 navLink.forEach(link => {
@@ -24,6 +25,8 @@ navLink.forEach(link => {
             link.style.background = '#a8a7b8';
         })
 })
+
+// Two
 // mouseout event
 // returns default style
 navLink.forEach(link => {
@@ -33,15 +36,39 @@ navLink.forEach(link => {
         link.style.padding = '0em';
     })
 })
+
+// Three
 // keydown event
 // alerts the key pressed
 window.addEventListener('keydown', (event) => {
     alert('You pressed the ' + event.key + ' key!');
 })
+
+// Four
 // dblclick event
 // flip images on dblclick
 image.forEach(img => {
     img.addEventListener('dblclick', () => {
         img.style.transform = 'scaleX(-1)';
         })
+})
+
+// Five
+// scroll event
+// alert user when can't scroll further
+window.addEventListener('scroll', () => {
+    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+    if(scrolled === scrollable){
+        alert('You can\'t scroll any further!');
+    }
+})
+
+// Six
+// focus event
+// add red border on focus
+navLink.forEach(link => {
+    link.addEventListener('focus', () => {
+        link.style.border = '1px solid red'
+    })
 })
